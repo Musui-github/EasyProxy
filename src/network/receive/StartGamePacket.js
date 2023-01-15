9a518da0e47429e2bdb2714bb9cdd9e681fe8d29
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2023-01-15 01:26:30 +0100

    Add files via upload

--- src/network/receive/StartGamePacket.js
+++ src/network/receive/StartGamePacket.js
@@ -6,9 +6,9 @@ module.exports = {
         Player.setPosition({x: packet.params.player_position.x, y: packet.params.player_position.y, z: packet.params.player_position.z, world: packet.params.world_name, pitch: 0, yaw: 0});
         Player.setGamemode(packet.params.player_gamemode);
         Player.setUniqueID(packet.params.runtime_entity_id);
-        //Player.getWorldManager.setDimension(packet.params.dimension);
-        //Player.getWorldManager.setSpawnPosition(packet.params.spawn_position);
-        //Player.getWorldManager.setGamerule(packet.params.gamerules);
+        Player.getWorldManager().setDimension(packet.params.dimension);
+        Player.getWorldManager().setSpawnPosition(packet.params.spawn_position);
+        Player.getWorldManager().setGamerule(packet.params.gamerules);
         Player.setGameVersion(packet.params.game_version);
     }
 }
