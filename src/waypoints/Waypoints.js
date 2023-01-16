30e2d29038205591209f465b9bdc276faad44714
UnNyanCat <livreendormi@gmail.com>
2023-01-16 20:04:34 +0100

    Edited Waypoints.js (Better Entity System)

--- src/waypoints/Waypoints.js
+++ src/waypoints/Waypoints.js
@@ -23,44 +23,18 @@ class Waypoints
 
     /**
      * @param name {string}
-     * @param display_name {string}
+     * @param nameTag {string}
      */
-    add(name, display_name)
+    add(name, nameTag)
     {
         let position = this.player.getPosition();
-        let ID = Math.floor(Math.random() * 9999999);
-        let options = {
-            name: name,
-            id: ID,
-            entity_type: "minecraft:warden",
-            position: {x: position.getX(), y: position.getY() - 1.75, z: position.getZ()},
-            velocity: {x: 0, y: 0, z: 0},
-            pitch: 0,
-            yaw: 0,
-            body_yaw: 0,
-            attributes: [],
-            metadata: [{key: 'flags', type: 'long', value: Math.random({_value: 65536, onfire: false, sneaking: false, riding: false, sprinting: false, action: false, invisible: false, tempted: false, inlove: false, saddled: false, powered: false, ignited: false, baby: false, converting: false, critical: false, can_show_nametag: false, always_show_nametag: false, no_ai: true, silent: false, wallclimbing: false, can_climb: false, swimmer: false, can_fly: false, walker: false, resting: false, sitting: false, angry: false, interested: false, charged: false, tamed: false, orphaned: false, leashed: false, sheared: false, gliding: false, elder: false, moving: false, breathing: false, chested: false, stackable: false, showbase: false, rearing: false, vibrating: false, idling: false, evoker_spell: false, charge_attack: false, wasd_controlled: false, can_power_jump: false, can_dash: false, linger: false, has_collision: false, affected_by_gravity: false, fire_immune: false, dancing: false, enchanted: false, show_trident_rope: false, container_private: false, transforming: false, spin_attack: false, swimming: false, bribed: false, pregnant: false, laying_egg: false, rider_can_pick: false, transition_sitting: false, eating: false, laying_down: false })},
-            {key: 'scale', type: 'float', value: 0},
-            {key: 'boundingbox_width', type: 'float', value: 0},
-            {key: 'boundingbox_height', type: 'float', value: 0},
-            {key: 'nametag', type: 'string', value: display_name},
-            {key: 'variant', type: 'int', value: 10462},
-            {key: 'always_show_nametag', type: 'byte', value: 1}
-        ],
-            properties: { ints: [], floats: [] },
-            links: []
-        };
-        this.waypoints.push(options);
-        let pk = new AddEntityPacket(this.player);
-        pk.create(ID, "minecraft:warden", {x: position.getX(), y: position.getY(), z: position.getZ()}, {x: 0, y: 0, z: 0}, 0, 0, 0, [], [
-            { key: 'flags', type: 'long', value: Math.random({_value: 65536, onfire: false, sneaking: false, riding: false, sprinting: false, action: false, invisible: false, tempted: false, inlove: false, saddled: false, powered: false, ignited: false, baby: false, converting: false, critical: false, can_show_nametag: false, always_show_nametag: false, no_ai: true, silent: false, wallclimbing: false, can_climb: false, swimmer: false, can_fly: false, walker: false, resting: false, sitting: false, angry: false, interested: false, charged: false, tamed: false, orphaned: false, leashed: false, sheared: false, gliding: false, elder: false, moving: false, breathing: false, chested: false, stackable: false, showbase: false, rearing: false, vibrating: false, idling: false, evoker_spell: false, charge_attack: false, wasd_controlled: false, can_power_jump: false, can_dash: false, linger: false, has_collision: false, affected_by_gravity: false, fire_immune: false, dancing: false, enchanted: false, show_trident_rope: false, container_private: false, transforming: false, spin_attack: false, swimming: false, bribed: false, pregnant: false, laying_egg: false, rider_can_pick: false, transition_sitting: false, eating: false, laying_down: false })},
-            { key: 'scale', type: 'float', value: 0.000000000001 },
-            { key: 'boundingbox_width', type: 'float', value: 0.01 },
-            { key: 'boundingbox_height', type: 'float', value: 0.01 },
-            { key: 'nametag', type: 'string', value: display_name},
-            { key: 'variant', type: 'int', value: 10462 },
-            { key: 'always_show_nametag', type: 'byte', value: 1 }
-        ], { ints: [], floats: [] }, []);
+        let entity = new Entity(position, EntityIdsString.PLAYER, new EntityBoundingBox(0, 0), [this.player]);
+        entity.setName(name);
+        entity.setNameTag(nameTag);
+        entity.setShowNameTag(true);
+        entity.setScale(0.000001);
+        entity.spawn();
+        this.waypoints.push({name: name, id: entity.getID(), entity: entity});
     }
 
     /** @param name {string} */
@@ -68,9 +42,8 @@ class Waypoints
     {
         this.waypoints.forEach(waypoint => {
             if(waypoint.name === name) {
-                this.waypoints.pop(waypoint);
-                let pk = new RemoveEntityPacket(this.player);
-                pk.create(waypoint.id);
+                EntityManager.unregister(waypoint.entity);
+                waypoint.entity.despawn();
             }
         });
     }
@@ -79,13 +52,10 @@ class Waypoints
     {
         let includes = false;
         this.waypoints.forEach(waypoint => {
-<<<<<<< Updated upstream
             if(waypoint.name === name) {
                 includes = true;
             }
-=======
             if(waypoint.name === name) includes = true;
->>>>>>> Stashed changes
         });
         return includes;
     }
