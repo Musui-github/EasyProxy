64681d3529acc2a94ecbbfa250c4c9d3c4118caf
UnNyanCat <67955616+UnNyanCat@users.noreply.github.com>
2023-01-29 21:37:56 +0100

    Removed alias thing

--- src/network/receive/AvailableCommandsPacket.js
+++ src/network/receive/AvailableCommandsPacket.js
@@ -29,32 +29,6 @@ module.exports = {
                     }
                 }]]
             });
-
-            if(command.getAliases() !== null) {
-                command.getAliases().forEach((alias) => {
-                    packet.params.enum_values.push(`/${alias}`);
-                    packet.params.command_data.push({
-                        name: `/${alias}`,
-                        description: command.getDescription(),
-                        flags: 0,
-                        permission_level: 0,
-                        alias: -1,
-                        overloads: [[{
-                            parameter_name: "args",
-                            value_type: "raw_text",
-                            enum_type: "valid",
-                            optional: true,
-                            options: {
-                                unused: 0,
-                                collapse_enum: 0,
-                                has_semantic_constraint: 0,
-                                as_chained_command: 0,
-                                unknown2: 0
-                            }
-                        }]]
-                    });
-                });
-            }
         });
     }
 }
